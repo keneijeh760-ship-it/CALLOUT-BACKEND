989c8c3046205c7dc19f3d0445ebbbf084b84d07
Kene Ijeh <keneijeh760@gmail.com>
2026-03-21 20:02:45 +0100

    aws generate download url method

--- src/main/java/com/phope/realcalloutbackend/aws/s3/S3Service.java
+++ src/main/java/com/phope/realcalloutbackend/aws/s3/S3Service.java
@@ -1,13 +1,21 @@
 package com.phope.realcalloutbackend.aws.s3;
 
 import com.phope.realcalloutbackend.aws.s3.dto.PresignedUrlResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service {
+    private  final S3Presigner s3Presigner;
 
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
@@ -18,7 +26,23 @@ public class S3Service {
     public PresignedUrlResponse generateUploadUrl (String filename, String contentType){
         String objectKey = UUID.randomUUID() + "/" +  filename;
 
-        PutObject
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(expiryMinutes))
+                .putObjectRequest(putRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+
+        return new PresignedUrlResponse(
+                presignedRequest.url().toString(),
+                objectKey
+        );
     }
 
 }
