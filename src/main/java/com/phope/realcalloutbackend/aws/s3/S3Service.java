2adce8a958bead8b108c54b9aeb67a425c6643c2
Kene Ijeh <keneijeh760@gmail.com>
2026-03-21 20:06:26 +0100

    aws generate download url method

--- src/main/java/com/phope/realcalloutbackend/aws/s3/S3Service.java
+++ src/main/java/com/phope/realcalloutbackend/aws/s3/S3Service.java
@@ -4,8 +4,10 @@ import com.phope.realcalloutbackend.aws.s3.dto.PresignedUrlResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
@@ -43,6 +45,20 @@ public class S3Service {
                 presignedRequest.url().toString(),
                 objectKey
         );
+    }
+
+    public String generateDownloadUrl (String objectKey){
+        GetObjectRequest getRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(expiryMinutes))
+                .getObjectRequest(getRequest)
+                .build();
+
+        return s3Presigner.presignGetObject(presignRequest).url().toString();
     }
 
 }
